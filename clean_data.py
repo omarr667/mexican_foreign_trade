abb8b4ec1b0458f5b8c31394294bc89f38ea1754
omarr667 <50962383+omarr667@users.noreply.github.com>
2025-03-23 21:22:59 -0600

    refactor(clean_data.py): restructure data cleaning logic into a function
    The data cleaning logic was previously inline and repetitive. It has been refactored into a reusable function `clean_data` to improve maintainability and reduce redundancy. The function now handles column renaming, year and month extraction, and filtering in a more structured manner. Additionally, the database query logic was simplified to fetch table names directly.

--- clean_data.py
+++ clean_data.py
@@ -2,44 +2,46 @@ import sqlite3
 import pandas as pd
 
 
+def clean_data(table):
+    """
+    Clean data from mexico_comercio_exterior.sql
+    """
+        
+    meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+    df = pd.read_sql(f"SELECT * FROM {table}", conn)
+
+    # Agregar la columna 'year' usando mask y ffill
+    df['year'] = df['Año'].mask(df['Año'].isin(meses)).ffill()
+
+    # Filtrar las filas donde 'Año' no sea igual a 'year'
+    df = df[df['Año'].isin(meses)].copy()
+
+    # Crear la columna 'mes' con índice del mes correspondiente
+    df['mes'] = df['Año'].map(lambda x: meses.index(x) + 1)
+    df = df.rename(columns={'Año': 'Mes Nombre', 
+                            'year': 'Año',
+                            'mes': 'Mes'})
+
+    df = df[['Año', 'Mes', 'Exportaciones', 'Importaciones',  'Comercio Total',  'Balanza Comercial']]
+    return df
+
+
+
 
 conn = sqlite3.connect('data/mexico_comercio_exterior.db')
 cur = conn.cursor()
 
 
-
 # consulta todas las tablas que tienen el sufijo bcm_e
-cur.execute("SELECT * FROM sqlite_master where type = 'table' and name like 'bcm_e_%'")
+cur.execute("SELECT name FROM sqlite_master where type = 'table' and name like 'bcm_e_%'")
 
 tables = cur.fetchall()
 
-table = tables[0][1]
-
-meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
-df = pd.read_sql(f"SELECT * FROM {table}", conn)
-
-years = []
-year = ''
-for index, row in df.iterrows():
-    if row['Año'] not in meses:
-        year = row["Año"]
-    years.append(year)
-
-df['year'] = years
-
-df = df[df['year'] != df['Año']]
-
-
-
-
-
-
-
-def clean_data(df):
-    """
-    Clean data from mexico_comercio_exterior.sql
-    """
+for table in tables:
     
+    table_name = table[0]
+    df = clean_data(table_name)
+    df.to_sql(f"clean_{table_name}", conn, if_exists='replace', index=False)
 
 
 
